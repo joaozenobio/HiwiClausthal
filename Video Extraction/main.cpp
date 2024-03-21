bef63d249df52c2ea42a7e29a6ac6a33729ce31a
joaozenobio <joao.zenobio@aluno.ufop.edu.br>
2024-03-21 20:09:29 -0300

    donegit status

--- Video Extraction/main.cpp
+++ Video Extraction/main.cpp
@@ -5,15 +5,18 @@ int main() {
 
 	// Online settings
 	
-	int recording_duration = 15;
-	std::string base_path = "C:\\Users\\zenob\\Desktop\\output";
-	int num_devices = 3;
-	onlineExtraction(recording_duration, base_path, num_devices);
+	//int recording_duration = 15;
+	//std::string base_path = "C:\\Users\\zenob\\Desktop\\output";
+	//int num_devices = 2;
+	//onlineExtraction(recording_duration, base_path, num_devices);
 
 	// Playback settings
 
-	//std::string input_path = "C:\\Users\\zenob\\Desktop\\dataset\\task1\\person1\\front.mkv";
-	//playbackExtraction(input_path);
+	std::ifstream filein("C:\\Users\\zenob\\Desktop\\files.txt");
+	for (std::string input_path; std::getline(filein, input_path); )
+	{
+		playbackExtraction(input_path);
+	}
 
 	return 0;
 }
